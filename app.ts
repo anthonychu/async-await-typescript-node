3d67b7c39959a17d50b9b6667e26fabeafefaa5e
Anthony Chu <anthony@anthonychu.ca>
2016-01-04 22:26:11 -0800

    Change to use async/await

--- app.ts
+++ app.ts
@@ -1,33 +1,27 @@
 "use strict";
 import api = require("./api");
 
-const AVAILABLE = 'available';
-const BASE_URL = 'http://petstore.swagger.io/v2';
+(async () => {
+    const AVAILABLE = 'available';
+    const BASE_URL = 'http://petstore.swagger.io/v2';
 
-let petApi = new api.PetApi('', BASE_URL);
-let storeApi = new api.StoreApi('', BASE_URL);
+    let petApi = new api.PetApi('', BASE_URL);
+    let storeApi = new api.StoreApi('', BASE_URL);
 
-try {
-    storeApi.getInventory().then(inventoryResponse => {
-        let inventorySummary = inventoryResponse.body;
+    try {
+        let {body: inventorySummary} = await storeApi.getInventory();
+
         console.log('Pet Statuses:');
         for (let status of Object.keys(inventorySummary)) {
             console.log(`${status} - ${inventorySummary[status]}`);
         }
 
         if (inventorySummary[AVAILABLE] > 0) {
-            petApi.findPetsByStatus(AVAILABLE).then(petResponse => {
-                let availablePets = petResponse.body;
-                console.log('\n\nAvailable Pets:');
-                availablePets.map(p => console.log(`${p.id} - ${p.name}`));
-            }).catch(e => {
-                // if promise from findPetsByStatus() is rejected or exception thrown in callback
-            });
+            let {body: availablePets} = await petApi.findPetsByStatus(AVAILABLE);
+            console.log('\n\nAvailable Pets:');
+            availablePets.forEach(p => console.log(`${p.id} - ${p.name}`));
         }
-    }).catch(e => {
-        // if promise from getInventory() is rejected or exception thrown in callback
-    });
-} catch (e) {
-    // does this catch anything?
-}
-
+    } catch (e) {
+        // this should catch all exceptions
+    }
+})();
